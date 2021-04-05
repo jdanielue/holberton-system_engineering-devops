e781b1eee20dfc26804eb50da4c9062c1ba5f568
jdanielue <jdurregoe@gmail.com>
2021-04-05 13:59:42 -0500

    s100a1

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
-# Ensures that there is a holberton file in tmp
+# script that set up your client SSH configuration file
 file_line { '~/.ssh/config':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/holberton',
-  line   => 'PasswordAuthentication no',
+  line   => '   IdentityFile ~/.ssh/holberton',
+  line   => '   PasswordAuthentication no',
 }
